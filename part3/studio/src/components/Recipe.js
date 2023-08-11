57846bc4afa13addb02043bec2b57249735c3a6f
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-08-10 19:54:33 -0500

    commit

--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,11 +1,11 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.theppk.com/2021/01/earl-gray-chai-pancakes/";
+   let authorPhoto = "https://www.theppk.com/wp-content/uploads/2019/09/273381BB-7937-4FEB-936F-FD56AB604987-819x1024.jpg";
+   let authorName = "Isa Chandra Moskowitz";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Isa Moskowitz" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -15,16 +15,15 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
-   return(
+   const ingredients = ["Earl Gray Tea", "All Purpose Flower", "Ground Cinnamon", "Baking Powder", "Soy Milk"];
+
+   return (
       <div>
          <h3>Recipe Ingredients</h3>
          <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
+            {ingredients.map((ingredient, index) => (
+               <li key={index}>{ingredient}</li>
+            ))}
          </ul>
       </div>
    );
@@ -34,8 +33,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Earl Gray Chai Pancakes </h1>
+            <p>These pancakes have it all! Masala chai spices like ginger, cardamom and black pepper. And although I’ve made chai spiced pancakes before, this time I thought of adding actual tea. Like wow, how brilliant, since “tea” is what chai is. You can use any black tea you like.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +46,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://www.theppk.com/wp-content/uploads/2021/01/ChaiPancake-copy-1.jpg" alt="Earl Gray Chai Pancakes" className="imageUpdates"/>
    );
 }
 
